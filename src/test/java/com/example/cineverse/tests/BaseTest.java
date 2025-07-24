7c10c45d676acb9b848d2518e6115af5c855cc96
jaabless <jefferyasamoahbless@gmail.com>
2025-07-24 14:18:48 +0000

    Add BaseTest with WebDriver setup and teardown configuration

--- src/test/java/com/example/cineverse/tests/BaseTest.java
+++ src/test/java/com/example/cineverse/tests/BaseTest.java
@@ -1,7 +1,6 @@
 package com.example.cineverse.tests;
 
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.example.cineverse.pages.HomePage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
