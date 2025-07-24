89e41232cb5f257e81770a8ee7112646d4054a99
jaabless <jefferyasamoahbless@gmail.com>
2025-07-24 14:09:05 +0000

    Add BaseTest with WebDriver setup and teardown configuration

---
+++ src/test/java/com/example/cineverse/tests/BaseTest.java
@@ -0,0 +1,44 @@
+package com.example.cineverse.tests;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.example.cineverse.pages.HomePage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected HomePage homePage;
+
+    @BeforeEach
+    public void setupDriver() {
+        // Detect OS
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")) {
+            System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        }
+
+        ChromeOptions options = new ChromeOptions();
+        if (!os.contains("win")) {
+            options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+        }
+
+        driver = new ChromeDriver(options);
+        driver.get("https://d101mapcha7bof.cloudfront.net/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        homePage = new HomePage(driver);
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
